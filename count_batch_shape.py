0d8d063d113bc2117412388953923de4dc04ab2f
RaushanSingh-codes <raushan.kumar_cs.aiml24@gla.ac.in>
2026-03-24 21:43:29 +0530

    removing unneccessary files

--- count_batch_shape.py
+++
@@ -1,4 +0,0 @@
-path = 'food_freshness_model/config.json'
-with open(path, 'r', encoding='utf-8') as f:
-    data = f.read()
-print('count batch_shape:', data.count('"batch_shape"'))
--- inspect_weights2.py
+++
@@ -1,8 +0,0 @@
-import h5py
-import os
-
-p = os.path.join(r"c:\Users\Pc\Documents\project", "food_freshness_model", "model.weights.h5")
-with h5py.File(p, "r") as f:
-    print("root keys:", list(f.keys()))
-    print("layers sample:", list(f['layers'].keys())[:20])
-    print("conv-like:", [k for k in f['layers'].keys() if 'Conv' in k or 'conv' in k][:20])
